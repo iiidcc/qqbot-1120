611728837f65427ccc54c5ac4ac74bab9f0d3c71
Bulletgod <83668321+Bulletgod@users.noreply.github.com>
2022-01-23 21:19:58 +0800

    1120

---
+++ app/scripts/ql/yl.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+cd /ql/scripts
+pnpm add -g pnpm
+pnpm install date-fns
+pnpm install ts-md5
+pnpm install axios
+pnpm install download
+pnpm install fs
+pnpm install child_process	
+pnpm install crypto-js
+pnpm install crypto
+pnpm install dotenv
+pnpm install got
+pnpm install http-server
+pnpm install png-js
+pnpm install qrcode-terminal
+pnpm install request
+pnpm install tough-cookie
+pnpm install tslib
+pnpm install tunnel
+pnpm install md5	
+pnpm install jsdom
+pnpm install silly-datetime		
+pnpm install form-data
+pip3 install requests
+pnpm install js-base64
